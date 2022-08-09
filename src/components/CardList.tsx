bff76a49e66ef706547b1347e1743a5b0be28788
Felipe Bastos | DT3 <felipe.bastos@dt3.com.br>
2022-08-09 09:58:57 -0300

    :rocket: finish: finalizando desafio

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -16,17 +16,29 @@ interface CardsProps {
 }
 
 export function CardList({ cards }: CardsProps): JSX.Element {
+  const { isOpen, onClose, onOpen } = useDisclosure();
   // TODO MODAL USEDISCLOSURE
 
   // TODO SELECTED IMAGE URL STATE
+  const [selectedImageUrl, setSelectedImageUrl] = useState('');
 
   // TODO FUNCTION HANDLE VIEW IMAGE
+  const handleViewImage = (imageUrl: string): void => {
+    setSelectedImageUrl(imageUrl);
+    onOpen();
+  }
 
   return (
     <>
       {/* TODO CARD GRID */}
+      <SimpleGrid columns={[1, 2, 3]} spacing={8}>
+        {cards.map(card => (
+          <Card key={card.id} data={card} viewImage={handleViewImage} />
+        ))}
+      </SimpleGrid>
 
       {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage isOpen={isOpen} onClose={onClose} imgUrl={selectedImageUrl} />
     </>
   );
 }
